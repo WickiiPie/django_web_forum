0c8d60928dabaa0509484fb23dff5c87f907bee5
WickiiPie <wichapolp@hotmail.com>
2021-01-21 23:26:42 +0700

    Newly created .env file
    To make it work with database settings in settings.py
    
    Added      djangoforum/.env
    Deleted    djangoforum/__pycache__/__init__.cpython-38.pyc
    Deleted    djangoforum/__pycache__/settings.cpython-38.pyc
    Deleted    djangoforum/__pycache__/urls.cpython-38.pyc
    Deleted    djangoforum/__pycache__/wsgi.cpython-38.pyc
    Modified   djangoforum/settings.py

--- djangoforum/settings.py
+++ djangoforum/settings.py
@@ -10,6 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+# ANCHOR this is env file
+
+import os
+
+import environ
+env = environ.Env()
+environ.Env.read_env()  # reading .env file
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,10 +84,26 @@ WSGI_APPLICATION = 'djangoforum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+
+        # needs .env file (newly created)
+        'NAME': env.str("NAME"),
+        'USER': env.str("USER"),
+        'PASSWORD': env.str("PASSWORD"),
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+            'init_command': 'SET '
+            'character_set_connection=utf8,'
+            'collation_connection=utf8_bin'
+        },
+        'TEST_CHARSET': 'utf8',
+        'TEST_COLLATION': 'utf8_general_ci',
     }
 }
+
 
 
 # Password validation
