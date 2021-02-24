4ef4586762c4913888c6e0e54a2715ce60a88ae4
WickiiPie <wichapolp@hotmail.com>
2021-02-24 10:46:09 +0700

    Updated api/urls.py, api/views.py
    Updated api_view
    
    Modified   simpleforum/api/urls.py
    Modified   simpleforum/api/views.py

--- simpleforum/api/urls.py
+++ simpleforum/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from simpleforum.api.views import thread_list_create_api_view
+from simpleforum.api.views import (
+    thread_list_create_api_view,
+    thread_detail_api_view
+)
 
 
 urlpatterns = [
-    path("thread_api/", thread_list_create_api_view, name="thread_list_create_api_view")
+    path("thread_api/", thread_list_create_api_view, name="thread_list_create_api_view"),
+    path("thread_api/<int:pk>/", thread_detail_api_view, name="thread_detail_api_view"),
 ]
--- simpleforum/api/views.py
+++ simpleforum/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from simpleforum.models import Thread
 from simpleforum.api.serializers import ThreadSerializer
+from django.shortcuts import get_object_or_404
 
 @api_view(["GET", "POST"])
 def thread_list_create_api_view(request):
@@ -12,9 +13,36 @@ def thread_list_create_api_view(request):
         serializer = ThreadSerializer(threads, many=True)
         return Response(serializer.data)
 
+    # use built in django restframework
     elif request.method == "POST":
         serializer = ThreadSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def thread_detail_api_view(request, pk):
+    try:
+        thread = Thread.objects.get(pk=pk)
+    except Thread.DoesNotExist:
+        return Response({"error": {
+                            "code": 404,
+                            "message": "thread not found!"
+                        }}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = ThreadSerializer(thread)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = ThreadSerializer(thread, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        thread.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
